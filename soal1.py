72b61a4048830cebdd88e2af6f286d7de928b4fb
taufiq72 <taufiqq1207@gmail.com>
2026-09-22 10:20:11 +0700

    Soal 1

---
+++ soal1.py
@@ -0,0 +1,12 @@
+persentase = float(input("Masukkan persentase siswa (%): "))
+
+if persentase >= 90:
+    print("Excellent performance")
+elif persentase >= 80:
+    print("Very Good performance")
+elif persentase >= 70:
+    print("Good performance")
+elif persentase >= 60:
+    print("Average performance")
+else:
+    print("Performance di bawah rata-rata / Perlu peningkatan")
